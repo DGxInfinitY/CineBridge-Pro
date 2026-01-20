2d7b633d9bdcb175274533a4d1505f0459316598
Donovan Goodwin <ddg2goodwin@gmail.com>
2026-01-19 22:31:31 -0700

    Bump Version to v4.17.6 (Dev)

--- tools/build_linux_local.sh
+++ tools/build_linux_local.sh
@@ -78,7 +78,7 @@ EOF
 
 # Create Control File
 # Get version from version info or default
-VERSION="4.17.5" 
+VERSION="4.17.6" 
 cat <<EOF > dist/deb/DEBIAN/control
 Package: cinebridgepro
 Version: $VERSION
